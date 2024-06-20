3eacda40750dbda151d069ca9f4a751f9ace3fc8
JinSeoHyun <15roa1121@gmail.com>
2024-06-20 14:37:05 +0900

    프로젝트 시작

---
+++ src/test/java/com/jsh/oauth_jwt_study/OauthJwtStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsh.oauth_jwt_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
